4e288f59a7fe44690940df15e2b68a05188587b4
Cory <cory@kreatednine.com>
2013-03-09 16:48:45 -0500

    added parser class which is now loaded from the main zjs object. added a utility lib for misc functions

--- test/zjs.js
+++ test/zjs.js
@@ -3,9 +3,13 @@
  */
 
 var zjs = require('..');
-
+	util = require('../lib/util');
 
 // render a string
+/* 
 zjs.renderFile('./test/views/view.zjs', {}, function(err, result){
 	console.log(result);	
 });
+*/
+
+log(zjs.parse('blahblah'));
